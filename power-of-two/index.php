37abdf08b7050781279c462c71ee642dd50acd9e
suffjasteb <suffceein12@gmail.com>
2025-02-23 18:07:18 +0700

    Implement proper power of two check with bitwise operator

--- power-of-two/index.php
+++ power-of-two/index.php
@@ -1,13 +1,17 @@
 <?php
 
 function validationNumber($n) {
-    if ($n % 2 == 0) {
-        echo "$n is genap\n";
+    if ($n > 0 && ($n & ($n - 1)) == 0) {
+        echo "$n is power of 2\n";
     } else {
-        echo "$n is ganjil\n";
+        echo "$n is not power of 2\n";
     }
 }
 
-validationNumber(3);
-validationNumber(10);
+validationNumber(3);  // 3 is not power of 2
+validationNumber(4);  // 4 is power of 2
+validationNumber(10); // 10 is not power of 2
+validationNumber(16); // 16 is power of 2
+validationNumber(32); // 32 is power of 2
+validationNumber(60);
 ?>
